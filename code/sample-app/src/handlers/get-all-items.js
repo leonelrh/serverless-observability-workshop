e69720b8ccebab599521393a4434329c0c32bcfa
leonelrh <wilmerleonel1717@gmail.com>
2026-06-23 02:39:28 +0000

    test

--- code/sample-app/src/handlers/get-all-items.js
+++ code/sample-app/src/handlers/get-all-items.js
@@ -1,10 +1,20 @@
 const AWS = require('aws-sdk')
 const docClient = new AWS.DynamoDB.DocumentClient()
+const { Unit } = require("aws-embedded-metrics");
+const { logMetricEMF } = require('../lib/logging/logger')
+
+let _cold_start = true
 
 exports.getAllItemsHandler = async (event, context) => {
     let response
     try {
+        if (_cold_start) {
+            //Metrics
+            await logMetricEMF('ColdStart', unit = Unit.Count, 1, { service: 'item_service', function_name: context.functionName })
+            _cold_start = false
+        }
         if (event.httpMethod !== 'GET') {
+            await logMetricEMF('UnsupportedHTTPMethod', unit = Unit.Count, 1, { service: 'item_service', operation: 'get-all-items' })
             throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`)
         }
 
@@ -16,6 +26,8 @@ exports.getAllItemsHandler = async (event, context) => {
             },
             body: JSON.stringify(items)
         }
+        //Metrics
+        await logMetricEMF('SuccessfulGetAllItems', unit = Unit.Count, 1, { service: 'item_service', operation: 'get-all-items' })
     } catch (err) {
         response = {
             statusCode: 500,
@@ -24,20 +36,9 @@ exports.getAllItemsHandler = async (event, context) => {
             },
             body: JSON.stringify(err)
         }
+        //Metrics
+        await logMetricEMF('FailedGetAllItems', unit = Unit.Count, 1, { service: 'item_service', operation: 'get-all-items' })
     }
-    return response
-}
-
-
-const getAllItems = async () => {
-    let response
-    try {
-        var params = {
-            TableName: process.env.SAMPLE_TABLE
-        }
-        response = await docClient.scan(params).promise()
-    } catch (err) {
-        throw err
-    }
+        
     return response
 }
--- code/sample-app/src/handlers/get-by-id.js
+++ code/sample-app/src/handlers/get-by-id.js
@@ -1,47 +1,45 @@
 const AWS = require('aws-sdk')
 const docClient = new AWS.DynamoDB.DocumentClient()
+const { Unit } = require("aws-embedded-metrics");
+const { logMetricEMF } = require('../lib/logging/logger')
+
+let _cold_start = true
 
 exports.getByIdHandler = async (event, context) => {
-  let response, id
-  try {
-    if (event.httpMethod !== 'GET') {
-      throw new Error(`getById only accept GET method, you tried: ${event.httpMethod}`)
+    let response, id
+    try {
+        if (_cold_start) {
+            //Metrics
+            await logMetricEMF('ColdStart', unit = Unit.Count, 1, { service: 'item_service', function_name: context.functionName })
+            _cold_start = false
+        }
+        if (event.httpMethod !== 'GET') {
+            await logMetricEMF('UnsupportedHTTPMethod', unit = Unit.Count, 1, { service: 'item_service', operation: 'get-by-id' })
+            throw new Error(`getById only accept GET method, you tried: ${event.httpMethod}`)
+        }
+
+        id = event.pathParameters.id
+        const item = await getItemById(id)
+        response = {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(item)
+        }
+        //Metrics
+        await logMetricEMF('SuccessfulGetItem', unit = Unit.Count, 1, { service: 'item_service', operation: 'get-by-id' })
+    } catch (err) {
+        response = {
+            statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(err)
+        }
+        //Metrics
+        await logMetricEMF('FailedGetItem', unit = Unit.Count, 1, { service: 'item_service', operation: 'get-by-id' })
     }
-
-    id = event.pathParameters.id
-    const item = await getItemById(id)
-
-    response = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify(item)
-    }
-  } catch (err) {
-    response = {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify(err)
-    }
-  }
-  return response
-}
-
-
-const getItemById = async (id) => {
-  let response
-  try {
-    var params = {
-      TableName: process.env.SAMPLE_TABLE,
-      Key: { id: id }
-    }
-
-    response = await docClient.get(params).promise()
-  } catch (err) {
-    throw err
-  }
-  return response
+        
+    return response
 }
