9fbfde2c11a1e47c09a7f62e422fb648afc15e6c
leonelrh <wilmerleonel1717@gmail.com>
2026-06-23 02:19:01 +0000

    test

--- code/sample-app/src/handlers/put-item.js
+++ code/sample-app/src/handlers/put-item.js
@@ -1,50 +1,43 @@
 const AWS = require('aws-sdk')
 const docClient = new AWS.DynamoDB.DocumentClient()
+const { MetricUnit } = require('../lib/helper/models')
+const { putMetric } = require('../lib/logging/logger')
+
+let _cold_start = true
 
 exports.putItemHandler = async (event, context) => {
     let response
     try {
+        if (_cold_start) {
+            //Metrics
+            await putMetric('ColdStart', unit = MetricUnit.Count, value = 1, { service: 'item_service', function_name: context.functionName })
+            _cold_start = false
+        }
         if (event.httpMethod !== 'POST') {
+            await putMetric('UnsupportedHTTPMethod', unit = MetricUnit.Count, value = 1, { service: 'item_service', operation: 'put-item' })
             throw new Error(`PutItem only accept POST method, you tried: ${event.httpMethod}`)
         }
 
         const item = await putItem(event)
-
         response = {
             statusCode: 200,
             headers: {
                 'Access-Control-Allow-Origin': '*'
             },
-            body: JSON.stringify(item)
+          body: JSON.stringify(item)
         }
+        //Metrics
+        await putMetric('SuccessfulPutItem', unit = MetricUnit.Count, value = 1, { service: 'item_service', operation: 'put-item' })
     } catch (err) {
         response = {
             statusCode: 500,
             headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify(err)
+              'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify(err)
         }
-    }
-    return response
-}
-
-const putItem = async (event) => {
-    let response
-    try {
-        const body = JSON.parse(event.body)
-        const id = body.id
-        const name = body.name
-
-        var params = {
-            TableName: process.env.SAMPLE_TABLE,
-            Item: { id: id, name: name }
-        }
-
-        response = await docClient.put(params).promise()
-
-    } catch (err) {
-        throw err
+        //Metrics
+        await putMetric('FailedPutItem', unit = MetricUnit.Count, value = 1, { service: 'item_service', operation: 'put-item' })
     }
     return response
 }
